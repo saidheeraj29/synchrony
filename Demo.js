c99dd5ac456f7694288b6c0ce5c97346bcef91b7
saidheeraj29 <saidheeraj.kamakshi@gmail.com>
2021-04-05 12:36:17 +0000

    demo

--- Demo.js
+++ Demo.js
@@ -1,1 +1,35 @@
 
+console.log("hello dheeraj")
+
+let x=30;
+let y=40;
+
+if(x>y){
+    console.log("x is greater");
+}else if(y>x){
+    console.log("y is greater");
+}else{
+    console.log("both are equal");
+}
+
+// swith case
+
+let statecode= "hr"
+switch(statecode){
+    case "ts":
+    console.log("hyderabad");
+    break;
+    case "tn":
+    console.log("chennai");
+    break;
+    case "ka":
+    console.log("bangalore");
+    break;
+    case "ap":
+    console.log("vizag");
+    break;
+    default:
+        console.log("statecode not found")
+        break;
+
+}
