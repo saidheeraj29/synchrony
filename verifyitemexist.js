bcc4c8d49006f8a0460a91cbf75b20ae432333db
saidheeraj29 <saidheeraj.kamakshi@gmail.com>
2021-04-08 10:06:28 +0000

    verifyitem

---
+++ verifyitemexist.js
@@ -0,0 +1,18 @@
+let states = ["ts","ap","ka","dl","mh"];
+
+console.log(isvalueExist(states,"dl"));
+console.log(isvalueExist(states,"kl"));
+console.log(isvalueExist(states,"ap"));
+
+function isvalueExist(1stData,exp) {
+    let isvalueFound=false
+for(let i=0;i<1stData.length;i++){
+    if(1stData[i]==exp){
+        isvaluefound = true;
+    
+    break;
+    }
+
+}
+
+}
